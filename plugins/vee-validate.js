b5f52558e3dd1ba8e08c84767788bf2a6c12934b
unknown <hazzemmossad@gmail.com>
2021-12-20 13:10:35 +0200

    frist commit

---
+++ plugins/vee-validate.js
@@ -0,0 +1,15 @@
+import { extend } from 'vee-validate'
+import * as rules from 'vee-validate/dist/rules'
+import { messages } from 'vee-validate/dist/locale/en.json'
+
+Object.keys(rules).forEach((rule) => {
+  extend(rule, rules[rule])
+})
+
+// with typescript
+for (let [rule, validation] of Object.entries(rules)) {
+  extend(rule, {
+    ...validation,
+    message: messages[rule], // assign message
+  })
+}
